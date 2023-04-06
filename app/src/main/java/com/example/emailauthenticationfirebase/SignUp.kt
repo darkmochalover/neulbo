161e20075d2e75ebb5c1e0bfc4bc354ea1e4ac42
Mahesh <mahesh@digitalflake.com>
2023-04-06 12:21:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/emailauthenticationfirebase/SignUp.kt
@@ -0,0 +1,60 @@
+package com.example.emailauthenticationfirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import com.example.emailauthenticationfirebase.databinding.ActivityMainBinding
+import com.example.emailauthenticationfirebase.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+
+class SignUp : AppCompatActivity() {
+
+    private lateinit var binding:ActivitySignUpBinding
+    private lateinit var firebaseAuth:FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth=FirebaseAuth.getInstance()
+
+//        val signUpButton=findViewById<AppCompatButton>(R.id.signUpButton)
+        binding.signUpButton.setOnClickListener {
+
+            val email=binding.emailSignupEt.text.toString()
+            val password=binding.passwordSignupEt.text.toString()
+            val confirmPassword=binding.confirmPasswordSignupEt.text.toString()
+
+            if(email.isNotEmpty()&&password.isNotEmpty()&&confirmPassword.isNotEmpty())
+            {
+                if(password==confirmPassword){
+                    firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            val intent= Intent(this,SignIn::class.java)
+                            startActivity(intent)
+                        }
+                        else{
+                            Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+                }
+                else
+                {
+                    Toast.makeText(this,"Password is not matching",Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                Toast.makeText(this,"Empty Fields Are Not Allowed!!",Toast.LENGTH_SHORT).show()
+            }
+
+
+
+        }
+    }
+}
