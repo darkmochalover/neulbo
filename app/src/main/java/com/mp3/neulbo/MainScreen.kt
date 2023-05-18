b433ce349a35f50600a8b5d8052434f1dc501530
NamJA334 <skawkddks@gachon.ac.kr>
2023-05-19 01:20:33 +0900

    일기 저장하면 비행기 날리는 애니메이션 추가

--- app/src/main/java/com/mp3/neulbo/MainScreen.kt
+++ app/src/main/java/com/mp3/neulbo/MainScreen.kt
@@ -1,15 +1,24 @@
 package com.mp3.neulbo
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
 import android.widget.ImageButton
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import kotlinx.coroutines.delay
 
 class MainScreen : AppCompatActivity() {
 
 
     private lateinit var add_button: ImageButton
     private lateinit var calendar_button: ImageButton
+    private lateinit var paper_plane: ImageView
+    private lateinit var plane: Animation
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +27,15 @@ class MainScreen : AppCompatActivity() {
 
         add_button = findViewById(R.id.add)
         calendar_button=findViewById(R.id.calendar)
+        paper_plane=findViewById(R.id.paper_plane)
+        plane=AnimationUtils.loadAnimation(this,R.anim.save_diary)
+        paper_plane.visibility= View.INVISIBLE
+        if(intent.hasExtra("diaryText")){
+            paper_plane.visibility= View.VISIBLE
+            paper_plane.startAnimation(plane)
+            paper_plane.visibility= View.INVISIBLE
 
+        }
         //일기 추가 버튼
         add_button.setOnClickListener {
             val intent = Intent(this, DiaryActivity::class.java)
@@ -31,6 +48,7 @@ class MainScreen : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
 
 
     }
