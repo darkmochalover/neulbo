d72b99664648f3212d0cfbf22a8187bd85d9e1d1
darkmochalover <dkwls1026@gachon.ac.kr>
2023-05-22 14:57:35 +0900

    Revert "오류 수정 완"
    This reverts commit 473c3071fa129220df2ce4b973c046ea033efe0e.

--- app/src/main/java/com/mp3/neulbo/utils/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.mp3.neulbo.utils
-
-object Constants {
-    const val API_BASE_URL = "ec2-3-39-9-85.ap-northeast-2.compute.amazonaws.com"
-    const val TAG: String = "로그"
-    val PREFERENCE = "template.android.TTogilgi"
-}
