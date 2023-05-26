1707189e8bffb37e63ca9137b679c41bfda76df8
NamJA334 <skawkddks@gachon.ac.kr>
2023-05-26 15:04:45 +0900

    비행기 누르면 메시지 창 띄우기 추가(근데 창이 안닫김)

--- app/src/main/java/com/mp3/neulbo/Message.kt
+++ app/src/main/java/com/mp3/neulbo/Message.kt
@@ -1,16 +1,20 @@
 package com.mp3.neulbo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.fragment.app.FragmentManager
 import com.mp3.neulbo.databinding.FragmentMessageBinding
 
 
 class Message : Fragment() {
     // TODO: Rename and change types of parameters
     lateinit var testBinding : FragmentMessageBinding
+    private lateinit var check:ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +28,16 @@ class Message : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         testBinding = FragmentMessageBinding.inflate(inflater, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+        check=view.findViewById(R.id.check)
+        check.setOnClickListener{
+            Log.d("ButtonClick", "Check button clicked")
+            val fragmentManager = requireActivity().supportFragmentManager
+            fragmentManager.beginTransaction().remove(this@Message).commit()
+        }
+        check.isClickable = true
+
 
         return testBinding.root
     }
