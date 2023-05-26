92e4c1bcb75c74aaed6032ddaedfb608cf4f3297
Kim Rina <krn2363@naver.com>
2023-05-26 15:23:03 +0900

    다이어리 내용 저장(컨텐츠랑 날짜)

--- app/src/main/java/com/mp3/neulbo/Diary.kt
+++ app/src/main/java/com/mp3/neulbo/Diary.kt
@@ -2,9 +2,11 @@ package com.mp3.neulbo
 
 class Diary {
     var content: String?= null
+    var Date: String?= null
 
-    constructor(content: String?) {
+    constructor(content: String?, Date: String?) {
         this.content = content
+        this.Date = Date
     }
     fun getContent(content: String): String{
         return content
@@ -16,10 +18,11 @@ class Diary {
     fun toMap(): Map<String, Any?>{
         val result = HashMap<String, Any?>()
         result["content"] = content
+        result["Date"] = Date
         return result
     }
     override fun toString(): String {
-        return "Diary{content='$content'}"
+        return "Diary{content='$content', Date='$Date'}"
     }
 
 
--- app/src/main/java/com/mp3/neulbo/DiaryActivity.kt
+++ app/src/main/java/com/mp3/neulbo/DiaryActivity.kt
@@ -5,7 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DiaryActivity : AppCompatActivity() {
 
@@ -13,6 +19,8 @@ class DiaryActivity : AppCompatActivity() {
     private lateinit var goback: ImageButton
     private lateinit var save:ImageButton
     private lateinit var edit:EditText
+    var auth : FirebaseAuth? = null
+
     var myRef = FirebaseDatabase.getInstance().reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,10 +32,9 @@ class DiaryActivity : AppCompatActivity() {
         save=findViewById(R.id.save)
         edit=findViewById(R.id.diaryEditText)
 
+        auth = Firebase.auth
+        //val myUserId = uid
         //val user = Firebase.auth.currentUser
-
-
-
 
         //뒤로가기 버튼
         goback.setOnClickListener {
@@ -38,21 +45,30 @@ class DiaryActivity : AppCompatActivity() {
         //저장버튼
         save!!.setOnClickListener{
             //일기내용
+            val currentTime = getCurrentDateTime()
+            var uid = auth?.currentUser?.uid
             val input = edit!!.text.toString()
-            writeDiary("user",input)
+            if (uid != null) {
+                writeDiary(uid,input, currentTime)
+            }
 
             val intentSend = Intent(this, MainScreen::class.java)
             intentSend.putExtra("diaryText",input)
             startActivity(intentSend)
             finish()
         }
-
-
-
-
     }
-    private fun writeDiary(userId: String, content: String){
-        val diary = Diary(content)
-        myRef.push().child(userId.toString()).setValue(diary)
+
+    //현재 날짜와 시간 가져오는 함수
+    fun getCurrentDateTime(): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        val date = Date()
+        return dateFormat.format(date)
+    }
+    private fun writeDiary(userId: String, content: String, Date:String){
+        val diary = Diary(content, Date)
+
+        //setValue : 내용 초기화됨 (고쳐야 할듯)
+        myRef.child("user").child(userId).setValue(diary)
     }
 }
