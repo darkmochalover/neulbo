8017b015ecaf808cf40ae90080f145c3c2ca2e31
NamJA334 <skawkddks@gachon.ac.kr>
2023-05-30 04:52:13 +0900

    프로필 사진 감정에 따라 변경 기능 추가

--- app/src/main/java/com/mp3/neulbo/Profile.kt
+++ app/src/main/java/com/mp3/neulbo/Profile.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.data.PieData
@@ -17,6 +18,7 @@ class Profile : AppCompatActivity() {
 
     private lateinit var char: com.github.mikephil.charting.charts.PieChart
     private lateinit var goback: ImageButton
+    private lateinit var profile_pic:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
@@ -24,6 +26,7 @@ class Profile : AppCompatActivity() {
 
         char=findViewById(R.id.chart)
         goback=findViewById(R.id.goBack)
+        profile_pic=findViewById(R.id.profile_pic)
 
         //뒤로가기 버튼
         goback.setOnClickListener {
@@ -38,11 +41,31 @@ class Profile : AppCompatActivity() {
 
 // data Set
         val entries = ArrayList<PieEntry>()
-        entries.add(PieEntry(508f, "기쁨"))
-        entries.add(PieEntry(600f, "슬픔"))
-        entries.add(PieEntry(750f, "당황"))
-        entries.add(PieEntry(508f, "화남"))
-        entries.add(PieEntry(670f, "즐거움"))
+        entries.add(PieEntry(508f, "공포"))
+        entries.add(PieEntry(600f, "분노"))
+        entries.add(PieEntry(750f, "슬픔"))
+        entries.add(PieEntry(508f, "중립"))
+        entries.add(PieEntry(623f, "행복"))
+        entries.add(PieEntry(111f, "혐오"))
+        entries.add(PieEntry(243f, "놀람"))
+
+        var maxValue = Float.MIN_VALUE
+        var maxLabel=""
+        for (entry in entries) {
+            if (entry.value > maxValue) {
+                maxValue = entry.value
+                maxLabel = entry.label
+            }
+        }
+        when(maxLabel){
+            in "공포"->profile_pic.setImageResource(R.drawable.profile_fear_edit)
+            in "분노"->profile_pic.setImageResource(R.drawable.profile_angry_edit)
+            in "슬픔"->profile_pic.setImageResource(R.drawable.profile_sad_edit)
+            in "중립"->profile_pic.setImageResource(R.drawable.profile_basic_edit)
+            in "행복"->profile_pic.setImageResource(R.drawable.profile_happy_edit)
+            in "혐오"->profile_pic.setImageResource(R.drawable.profile_hate_edit)
+            in "놀람"->profile_pic.setImageResource(R.drawable.profile_surprised_edit)
+        }
         // add a lot of colors
         val colorsItems = ArrayList<Int>()
         for (c in ColorTemplate.VORDIPLOM_COLORS) colorsItems.add(c)
