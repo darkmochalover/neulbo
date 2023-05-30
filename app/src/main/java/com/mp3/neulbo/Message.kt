ec9da429d866f996b9dfca2efc8346b3a039a1e7
NamJA334 <skawkddks@gachon.ac.kr>
2023-05-30 19:08:33 +0900

    일기보는 변수 추가(text임)

--- app/src/main/java/com/mp3/neulbo/Message.kt
+++ app/src/main/java/com/mp3/neulbo/Message.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.mp3.neulbo.databinding.FragmentMessageBinding
@@ -14,6 +15,7 @@ import com.mp3.neulbo.databinding.FragmentMessageBinding
 class Message : Fragment() {
     lateinit var testBinding: FragmentMessageBinding
     private lateinit var check: ImageButton
+    private lateinit var text: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +23,8 @@ class Message : Fragment() {
     ): View? {
         testBinding = FragmentMessageBinding.inflate(inflater, container, false)
         val view = testBinding.root
+
+        text=text.findViewById(R.id.diary_view)
 
         check = view.findViewById(R.id.check)
         check.setOnClickListener {
